e861a694ff348cacfdc56209f9810a1f9b4e5b6e
reiko321 <reiko321@gmail.com>
2019-07-07 16:07:40 -0700

    syntax

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require("express-handlebars");
 const methodOverride = require('method-override');
 const routes = require("./controllers/burgers_controller");
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 // Config Settings
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,5 +17,5 @@ app.use("/", routes);
 app.use(express.static('public'));
 // Listener
 app.listen(PORT,function(){
-    console.log("App now listening at localhost:" + PORT;
+    console.log("App now listening at localhost:" + PORT);
 });
