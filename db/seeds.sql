d03d2d82ca04a9766a9bc01501c9f3f75fbf19c1
reiko321 <reiko321@gmail.com>
2019-07-07 15:53:59 -0700

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('All-American');
+
+INSERT INTO burgers(burger_name)
+VALUES('Heart Attack');
+
+INSERT INTO burgers(burger_name)
+VALUES('Bacon Cheeseburger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Impossible Burger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Keto Burger');
